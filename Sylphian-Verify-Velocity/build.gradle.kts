7e2c687c34a2f032990f59b128f962a3025cb4a7
QuackieMackie <dev.quackiemackie@gmail.com>
2026-05-29 22:02:25 +0100

    chore(build): remove `run-velocity` plugin configuration

--- Sylphian-Verify-Velocity/build.gradle.kts
+++ Sylphian-Verify-Velocity/build.gradle.kts
@@ -1,18 +1,5 @@
 // Sylphian-Verify-Velocity
-plugins {
-    id("xyz.jpenilla.run-velocity") version "3.0.2"
-}
-
 dependencies {
     compileOnly("com.velocitypowered:velocity-api:3.5.0-SNAPSHOT")
     annotationProcessor("com.velocitypowered:velocity-api:3.5.0-SNAPSHOT")
-}
-
-tasks {
-  runVelocity {
-    // Configure the Velocity version for our task.
-    // This is the only required configuration besides applying the plugin.
-    // Your plugin's jar (or shadowJar if present) will be used automatically.
-    velocityVersion("3.5.0-SNAPSHOT")
-  }
 }
