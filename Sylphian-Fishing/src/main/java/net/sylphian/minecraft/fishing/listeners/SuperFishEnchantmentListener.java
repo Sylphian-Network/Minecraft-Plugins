e53dd73f54911395af58f2c82897a6d6898c60de
QuackieMackie <dev.quackiemackie@gmail.com>
2026-05-29 22:00:39 +0100

    refactor(listener): simplify SuperFishEnchantment initialization logic

--- Sylphian-Fishing/src/main/java/net/sylphian/minecraft/fishing/listeners/SuperFishEnchantmentListener.java
+++ Sylphian-Fishing/src/main/java/net/sylphian/minecraft/fishing/listeners/SuperFishEnchantmentListener.java
@@ -7,7 +7,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
-import org.bukkit.inventory.ItemStack;
 
 import static io.papermc.paper.registry.RegistryAccess.registryAccess;
 import static net.sylphian.minecraft.fishing.SylphianFishingBootstrap.SUPER_FISH_KEY;
@@ -15,23 +14,21 @@ import static net.sylphian.minecraft.fishing.SylphianFishingBootstrap.SUPER_FISH
 public class SuperFishEnchantmentListener implements Listener {
     
     private final ConfigLoader config;
+    private final Enchantment superFishEnchantment;
 
     public SuperFishEnchantmentListener(ConfigLoader config) {
         this.config = config;
+        this.superFishEnchantment = registryAccess()
+                .getRegistry(RegistryKey.ENCHANTMENT)
+                .get(SUPER_FISH_KEY);
     }
 
     @EventHandler
     public void onConsume(PlayerItemConsumeEvent event) {
-        ItemStack item = event.getItem();
-        Player player = event.getPlayer();
+        if (superFishEnchantment == null) return;
+        if (!event.getItem().getEnchantments().containsKey(superFishEnchantment)) return;
 
-        Enchantment superFish = registryAccess()
-                .getRegistry(RegistryKey.ENCHANTMENT)
-                .get(SUPER_FISH_KEY);
-
-        if (superFish == null || !item.getEnchantments().containsKey(superFish)) return;
-
-        applySuperFishEffects(player);
+        applySuperFishEffects(event.getPlayer());
     }
 
     private void applySuperFishEffects(Player player) {
