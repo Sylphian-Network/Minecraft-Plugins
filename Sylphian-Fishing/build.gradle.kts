3f14632c0f48688ca460b0b578b1702a4f8a8964
QuackieMackie <dev.quackiemackie@gmail.com>
2026-05-29 22:01:55 +0100

    chore(build): remove unusable `run-paper` plugin configuration

--- Sylphian-Fishing/build.gradle.kts
+++ Sylphian-Fishing/build.gradle.kts
@@ -1,8 +1,4 @@
 // Sylphian-Fishing
-plugins {
-    id("xyz.jpenilla.run-paper") version "3.0.2"
-}
-
 dependencies {
     compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
     compileOnly(project(":Sylphian-Database"))
@@ -11,14 +7,6 @@ dependencies {
 }
 
 tasks {
-    runServer {
-        // Configure the Minecraft version for our task.
-        // This is the only required configuration besides applying the plugin.
-        // Your plugin's jar (or shadowJar if present) will be used automatically.
-        minecraftVersion("26.1.2")
-        jvmArgs("-Xms2G", "-Xmx2G", "-Dcom.mojang.eula.agree=true")
-    }
-
     processResources {
         val props = mapOf("version" to version )
         filesMatching("paper-plugin.yml") {
